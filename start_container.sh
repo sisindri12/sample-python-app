511ab9b867dbbb103b4dccd43c66a13d96f522b8
sisindri12 <59793688+sisindri12@users.noreply.github.com>
2024-04-21 23:20:56 +0530

    Pushing the start container script changes

--- start_container.sh
+++ start_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 #pull the container from hub
-docker pull sisindri1/sample-python-flask-app
+docker pull sisindri1/sample-python-flask-app:latest
 #run the container
 docker run -d -p 5000:5000 sisndri1/sample-python-flask-app
