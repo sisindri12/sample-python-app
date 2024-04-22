07a769a3e77561e67f586af06e5de37c995b50c6
sisindri12 <59793688+sisindri12@users.noreply.github.com>
2024-04-22 12:29:55 +0530

    Modified start_container script

--- start_container.sh
+++ start_container.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 #pull the container from hub
-# docker login -u "sisindri1" -p"Sisindri@1" "docker.io"
 docker pull sisindri1/sample-python-flask-app
-docker build . -t sisindri1/sample-python-flask-app
 #run the container
 docker run -d -p 5000:5000 sisndri1/sample-python-flask-app
